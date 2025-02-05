5d5d6cffa418e8faec1419356012984f12fbad80
nandini bagga <houseofarchdes@gmail.com>
2025-02-05 21:48:27 +0900

    msf

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -62,6 +62,146 @@ export default function Projects() {
       title: "Bedroom",
       src:       "/nine.png",
     },
+    {
+      title: "Bedroom",
+      src:       "/ten.png",
+    },
+    {
+      title: "Bedroom",
+      src:       "/eleven.png",
+    },
+    {
+      title: "Kid's Room",
+      src:       "/twelve.png",
+    },
+    {
+      title: "Kid's Room",
+      src:       "/thirteen.png",
+    },
+    {
+      title: "Living Room",
+      src:       "/forteen.png",
+    },
+    {
+      title: "Bedroom",
+      src:       "/fifteen.png",
+    },
+    {
+      title: "Living Room",
+      src:       "/eighteen.png",
+    },
+    {
+      title: "Bedroom",
+      src:       "/twenty.png",
+    },
+    {
+      title: "Living Room",
+      src:       "/twentyone.png",
+    },
+    {
+      title: "Dining Area",
+      src:       "/twentytwo.png",
+    },
+    {
+      title: "Kitchen",
+      src:       "/twentythree.png",
+    },
+    {
+      title: "Kitchen",
+      src:       "/twentyfour.png",
+    },
+    {
+      title: "Living Area",
+      src:       "/twentyfive.png",
+    },
+    {
+      title: "Living Area",
+      src:       "/twentysix.png",
+    },
+    {
+      title: "Bedroom",
+      src:       "/twentyseven.png",
+    },
+    {
+      title: "Bedroom",
+      src:       "/twentyeight.png",
+    },
+    {
+      title: "Bedroom",
+      src:       "/twentynine.png",
+    },
+    {
+      title: "Bedroom",
+      src:       "/thirty.png",
+    },
+    {
+      title: "Bedroom",
+      src:       "/thirtyone.png",
+    },
+    {
+      title: "Bedroom",
+      src:       "/thirtytwo.png",
+    },
+    {
+      title: "Table",
+      src:       "/thirtythree.png",
+    },
+    {
+      title: "Table",
+      src:       "/thirtythree.png",
+    },
+    {
+      title: "Living Area",
+      src:       "/thirtyfour.png",
+    },
+    {
+      title: "Living Area",
+      src:       "/thirtyfive.png",
+    },
+    {
+      title: "Jewellery Store",
+      src:       "/thirtysix.png",
+    },
+    {
+      title: "Jewellery Store",
+      src:       "/thirtyeight.png",
+    },
+    {
+      title: "Bedroom",
+      src:       "/forty.png",
+    },
+    {
+      title: "Bedroom",
+      src:       "/fortyone.png",
+    },
+    {
+      title: "Jewellery",
+      src:       "/fortytwo.png",
+    },
+    {
+      title: "Office",
+      src:       "/fortyfour.png",
+    },
+    {
+      title: "Office",
+      src:       "/fortyfive.png",
+    },
+    {
+      title: "Jewellery Store",
+      src:       "/fortysix.PNG",
+    },
+    {
+      title: "Living Area",
+      src:       "/fortynine.PNG",
+    },
+    {
+      title: "Living Area",
+      src:       "/fifty.PNG",
+    },
+    {
+      title: "Living Area",
+      src:       "/fiftytwo.PNG",
+    },
   ]
  
   return (
